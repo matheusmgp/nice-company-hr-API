05dc0b96fe8db2e08a204c16e8888defdd43650d
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-07 20:07:50 -0300

    FEAT: Pipes,Utils

--- src/modules/time-clock/pipes/check-if-cpf-exists.pipe.ts
+++ src/modules/time-clock/pipes/check-if-cpf-exists.pipe.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { ITimeClockService } from '../interfaces/timeclock.service.interface';
 import { CreateTimeClockDto } from '../dto/create-time-clock.dto';
+import { formatCpfOnlyNumber } from '@/shared/utils/cpf.util';
 
 @Injectable()
 export class CheckIfCpfExistsPipe implements PipeTransform {
@@ -14,7 +15,7 @@ export class CheckIfCpfExistsPipe implements PipeTransform {
     @Inject(ITimeClockService) private readonly service: ITimeClockService,
   ) {}
   async transform(value: CreateTimeClockDto, metadata: ArgumentMetadata) {
-    let result = await this.service.getByCpf(value.cpf);
+    let result = await this.service.getByCpf(formatCpfOnlyNumber(value.cpf));
     if (result) {
       throw new BadRequestException(`${value.cpf} já existe`);
     }
