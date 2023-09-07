e45eded56d20ff5a6fc9fdaee1c4339360af886a
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-07 10:39:11 -0300

    FEAT: EnvConfigModule ,PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { EnvConfigModule } from './shared/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/prisma/prisma.module.ts
@@ -0,0 +1,23 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+@Global()
+@Module({
+  imports: [],
+  providers: [PrismaService, ConfigService],
+  exports: [ConfigService],
+})
+export class PrismaModule {
+  static forTest(prismaClient: PrismaClient): DynamicModule {
+    return {
+      module: PrismaModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useFactory: () => prismaClient as PrismaService,
+        },
+      ],
+    };
+  }
+}
